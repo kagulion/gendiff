a83d91d685556f849137fcee5c0216dfc872912c
kagulion <kagulion@vk.com>
2026-01-30 09:36:55 +0300

    fix format case problem

--- src/getFormat.js
+++ src/getFormat.js
@@ -1,6 +1,6 @@
 import path from 'path'
 
-// Определение формата
-const getFormat = (filepath) => path.extname(filepath).slice(1)
+// Определение формата (регистронезависимо: .JSON, .YAML и т.д.)
+const getFormat = (filepath) => path.extname(filepath).slice(1).toLowerCase()
 
 export default getFormat
